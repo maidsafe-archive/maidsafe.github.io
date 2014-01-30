2e3096541a63f2264ed37480688045240cb9efe2
Scott-MacKenzie <scott.mackenzie@maidsafe.net>
2014-01-30 14:33:07 +0000

    Website files migrated over
    Former-commit-id: 6fe278267a6bb37dbc84d5326b042ba13c7db7b0

---
+++ licenses/index.php
@@ -0,0 +1,19 @@
+<?php $title="MaidSafe | Licenses"; ?>
+<?php $metakeywords="golifestuff, lifestuff, free cloud, free cloud storage, free online storage, file sharing, P2P file sharing, free file sync, online storage, file sharing, maidsafe, digital security, free file storage, free online backup"; ?>
+<?php $metadescription="MaidSafe creates user-friendly software that protects users digital privacy, security and freedom."; ?>
+<?php include('../includes/header.php'); ?>
+<body id="license">
+<?php include('../includes/nav.php'); ?>
+<div class="main-content clearfix">
+	<div class="shell mt40">
+    	<section>
+        <h1>Licenses</h1>
+            <ul class="licenses">
+                <li><a href="LICENSE.txt" target="_blank">LICENSE</a></li>
+                <li><a href="COPYING.txt" target="_blank">COPYING</a></li>
+                <li><a href="CONTRIBUTOR.txt">CONTRIBUTOR</a></li>
+            </ul>
+        </section>
+	</div>
+</div>
+<?php include('../includes/footer.php'); ?>
