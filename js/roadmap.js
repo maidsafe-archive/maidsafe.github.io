050962f5b431f1fa2521d5259ef8f21fdbb9c571
Paige-Peterson <ioptio@riseup.net>
2014-11-25 02:04:38 -0800

    remove internal test, final styling fixes

--- js/roadmap.js
+++ js/roadmap.js
@@ -1,5 +1,4 @@
 var maind="<p>Currently we are working through Testnet 2.</br>View the various stages of development in more detail... </p>";
-var test0d="<p>See what we completed in our internal testing stage before starting the global test network.</p>";
 var test1d="<p>See what we completed in Testnet 1.</p>";
 var test2d="<p>Track the progress of Testnet 2's main objectives below. </br> For a more technical analysis, see our <a href='https://maidsafe.atlassian.net' target='_blank'>Jira Roadmap</a>.";
 var test3d="<p>See what we have planned for Testnet 3.</p>";
@@ -7,7 +6,6 @@ var launchd="<p>The work doesn't stop at Beta. See what we have planned for laun
 
 $(document).ready(function () {
 	$(".backwards").click(function(){displayPhase(this, "Project SAFE Roadmap", maind, "roadmap", "phase-group");});
-	$("#testnet0").click(function(){displayPhase(this, "Internal Testnet Overview", test0d, "testnet0", "roadmap");});
 	$("#testnet1").click(function(){displayPhase(this, "Testnet 1 Overview", test1d, "testnet1", "roadmap");});
 	$("#testnet2").click(function(){displayPhase(this, "Testnet 2 Overview", test2d, "testnet2", "roadmap");});
 	$("#testnet3").click(function(){displayPhase(this, "Testnet 3 Overview", test3d, "testnet3", "roadmap");});
@@ -22,7 +20,16 @@ function displayPhase (obj, title, desc, show, hide){
 	$("div."+hide).addClass("hidden");
 	$("div."+show).removeClass("hidden");
 	$("#viewer").get(0).scrollIntoView();
+	if ($("#viewer").width()<500 && show=="testnet2"){
+		$(".phase-group .long1 h2").replaceWith("<h2>Reworking vault accounts to reduce account transfer, memory requirements and ability to handle much higher churn</h2>");
+		$(".phase-group .long2 h2").replaceWith("<h2>Advances in Routing group and quorum for security levels well beyond network size and increased sybil attack defence</h2>");
+	}
 }
+
+// boolean smallWidth(){
+// 	if  return true;
+// 	else return false;
+// }
 	
 // function display(obj, name, bio, twitter, wp){
 // 	if (!$(obj).hasClass("icon-active")){
