303676032829a2eb02a06a42f11ee0597f316a10
KrishnaIndia <krishnaof1988@gmail.com>
2015-02-04 17:26:21 +0530

    press article page interim commit

--- app/js/press_article_video.js
+++ app/js/press_article_video.js
@@ -1,4 +1,19 @@
-  // Basic FitVids Test
-$(".video-container").fitVids();
-// Custom selector and No-Double-Wrapping Prevention Test
 $(".video-container").fitVids({ customSelector: "iframe[src^='http://socialcam.com']"});
+
+$(document).ready(function() {
+  $('#tabs').tabulous({
+    effect: 'scale'
+  });
+
+  $('#tabs2').tabulous({
+    effect: 'slideLeft'
+  });
+
+  $('#tabs3').tabulous({
+    effect: 'scaleUp'
+  });
+
+  $('#tabs4').tabulous({
+    effect: 'flip'
+  });
+});
