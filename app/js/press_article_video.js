4178b7a2ae920bbd0252ccdf153a29567cf4d2d0
shankar <shankar21mail@gmail.com>
2015-02-04 16:57:00 +0530

    Press Articles Added

---
+++ app/js/press_article_video.js
@@ -0,0 +1,4 @@
+  // Basic FitVids Test
+$(".video-container").fitVids();
+// Custom selector and No-Double-Wrapping Prevention Test
+$(".video-container").fitVids({ customSelector: "iframe[src^='http://socialcam.com']"});
