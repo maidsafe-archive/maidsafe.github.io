d6af18c4da75260d4c420891700f4180bc38ff6f
KrishnaIndia <krishnaof1988@gmail.com>
2015-02-06 11:53:11 +0530

    deployment testing on github.io

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function (grunt) {
 
   var CONFIG = {
     owner: 'krishnaindia',
-    repo: 'DroneIntegration',
+    repo: 'krishnaindia.github.io',
     baseBranch: 'master'
   };
 
