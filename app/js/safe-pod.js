ec39d0091fc562571bf7c77b82c07ca0cd66797d
KrishnaIndia <krishnaof1988@gmail.com>
2015-02-11 19:10:18 +0530

    overlay integrated for safe-pod maps

--- app/js/safe-pod.js
+++ app/js/safe-pod.js
@@ -1,9 +1,13 @@
 /*globals $, document*/
 
 $(document).ready(function() {
+  var map = $('#big-map');
+  var overlay = $('div.overlay');
   var toggle = function() {
-    $('#big-map').fadeToggle();
+    map.fadeToggle();
+    overlay.fadeToggle();
   };
   $('#map-popup').click(toggle);
   $('#big-map .close_btn').click(toggle);
+  overlay.click(toggle);
 });
